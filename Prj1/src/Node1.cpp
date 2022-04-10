d1eaa95fc97bbbc15a72068c69f6f5cc1b75f76e
marcopaas <103110349+marcopaas@users.noreply.github.com>
2022-04-10 18:42:14 +0200

    rpm conversion

--- Prj1/src/Node1.cpp
+++ Prj1/src/Node1.cpp
@@ -18,6 +18,8 @@ public:
     n.getParam("/width", w);
     n.getParam("/cpr", cpr);
 
+    ROS_INFO("cpr %d", cpr);
+
 
     sub_reader = n.subscribe <sensor_msgs::JointState>("wheel_states", 1000, &Node1::readerCallback, this);
     vel_pub = n.advertise <geometry_msgs::TwistStamped>("cmd_vel", 1000);
@@ -36,10 +38,10 @@ public:
   position_3 = msg->position[3];
   position_4 = msg->position[2];
 
-  ROS_INFO("position_1: [%d]", position_1);
-  ROS_INFO("position_2: [%d]", position_2);
-  ROS_INFO("position_3: [%d]", position_3);
-  ROS_INFO("position_4: [%d]", position_4);
+  ROS_INFO("position_1: %d", position_1);
+  ROS_INFO("position_2: %d", position_2);
+  ROS_INFO("position_3: %d", position_3);
+  ROS_INFO("position_4: %d", position_4);
 
   if(flag==1){
     past_sec = sec;
@@ -56,6 +58,7 @@ public:
     delta_nsec = (nsec-past_nsec)*0.000000001;
 
     delta_t = delta_nsec+ (double) delta_sec;
+    ROS_INFO("delta_t: [%f]", delta_t);
 
     //calcolo dell' angolo
     delta_theta_1 = 2*M_PI/cpr*(position_1-past_position_1);
@@ -63,30 +66,32 @@ public:
     delta_theta_3 = 2*M_PI/cpr*(position_3-past_position_3);
     delta_theta_4 = 2*M_PI/cpr*(position_4-past_position_4);
 
-    ROS_INFO("delta_theta_1: [%f]", delta_theta_1);
-    ROS_INFO("delta_theta_2: [%f]", delta_theta_2);
-    ROS_INFO("delta_theta_3: [%f]", delta_theta_3);
-    ROS_INFO("delta_theta_4: [%f]", delta_theta_4);
+    ROS_INFO("delta_theta_1: %f", delta_theta_1);
+    ROS_INFO("delta_theta_2: %f", delta_theta_2);
+    ROS_INFO("delta_theta_3: %f", delta_theta_3);
+    ROS_INFO("delta_theta_4: %f", delta_theta_4);
 
-    //velocità angolari delle singole ruote
+    //velocità angolari delle singole ruote [rad/s]
     u1 = delta_theta_1/delta_t;
     u2 = delta_theta_2/delta_t;
     u3 = delta_theta_3/delta_t;
     u4 = delta_theta_4/delta_t;
 
-    ROS_INFO("u1: [%f]", u1);
-    ROS_INFO("u2: [%f]", u2);
-    ROS_INFO("u3: [%f]", u3);
-    ROS_INFO("u4: [%f]", u4);
+    ROS_INFO("-----RPM-----");
+    ROS_INFO("u1: %f", u1*60/(2*M_PI));
+    ROS_INFO("u2: %f", u2*60/(2*M_PI));
+    ROS_INFO("u3: %f", u3*60/(2*M_PI));
+    ROS_INFO("u4: %f", u4*60/(2*M_PI));
 
-    //velocità del robot
+    //velocità del robot [m/s e rad/s]
     w_bz = r/4/(l+w)*(-u1+u2+u3-u4);
     v_bx = r/4*(u1+u2+u3+u4);
     v_by = r/4*(-u1+u2-u3+u4);
 
-    ROS_INFO("v_bx: [%f]", v_bx);
-    ROS_INFO("v_by: [%f]", v_by);
-    ROS_INFO("w_bz: [%f]", w_bz);
+
+    ROS_INFO("v_bx: %f", v_bx);
+    ROS_INFO("v_by: %f", v_by);
+    ROS_INFO("w_bz: %f", w_bz);
 
     //creo messaggio di tipo geometry_msgs/TwistStamped
     geom_msg.header.frame_id = "Robot velocities";
