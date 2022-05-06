38b58ff1d5c48dcd1d4d8c076db5ed910b79e343
AlessandroLux <103134676+AlessandroLux@users.noreply.github.com>
2022-05-06 18:00:07 +0200

    Update Node4.cpp

--- Prj1/src/Node4.cpp
+++ Prj1/src/Node4.cpp
@@ -31,48 +31,57 @@ public:
     n.getParam("/lenght", l);
     n.getParam("/width",  w);
     // ROS_INFO("%d",i);
-    i=0;
-    flag1 = 1;
-    flag2 = 1;
+    flag=1;
     P=P_;
   }
 
   void Callback(const sensor_msgs::JointState::ConstPtr& msg_bag)
   {  
-    if(flag1 == 1 && flag2 == 1) {
-      flag1 = 0;
-       } 
-    else if(flag1 == 0 && flag2 == 1){
-      flag2 = 0;
-       } 
+    
+     if (flag==1)
+     {
+       T1=msg_bag->header.stamp.sec;
+       flag=0;
+     }
      else {
+      T2=msg_bag->header.stamp.sec;
+      DeltaT=T2-T1;
+      flag=0;
+     }
+      
+      
       u1_bag=msg_bag->velocity[0]; // fl
       u2_bag=msg_bag->velocity[1]; // fr
       u4_bag=msg_bag->velocity[2]; // rl
       u3_bag=msg_bag->velocity[3]; // rr
       
-      if (P==1){
+      if (P==1 && DeltaT<=100){
       vvb.vbag = r/4/(l+w)*(-u1_bag+u2_bag+u3_bag-u4_bag);  
+      v_bag.publish(vvb);
       }
-      else if(P==0){
+      else if(P==0 && DeltaT<=13){
       vvb.vbag = r/4*(u1_bag+u2_bag+u3_bag+u4_bag);
+      v_bag.publish(vvb);
       }
       
-      v_bag.publish(vvb);
-     }
-  }
+      
+      
+         
+    }
+  
 
 void chatterCallback(const geometry_msgs::TwistStamped::ConstPtr& msg){
         
-        if (P==1){
+        if (P==1 && DeltaT<=100){
         vvc.vcomputed= msg->twist.angular.z;  
-        }
-        else if (P==0){
-        vvc.vcomputed= msg->twist.linear.x;
-        }
-
         vvc.vcomputed=vvc.vcomputed*60*5;
         v_sub.publish(vvc);
+        }
+        else if (P==0 && DeltaT<=13){
+        vvc.vcomputed= msg->twist.linear.x;
+        vvc.vcomputed=vvc.vcomputed*60*5;
+        v_sub.publish(vvc);
+        }
          
  }
 
@@ -98,26 +107,13 @@ private:
    float u2_bag; 
    float u3_bag; 
    float u4_bag; 
-
-   float u1_computed; 
-   float u2_computed; 
-   float u3_computed; 
-   float u4_computed; 
-
-  int i;
-  float vx_computed;
-  float vy_computed;
-  float wz_computed;
-
-  float vx_bag;
-  float vy_bag;
-  float wz_bag;
-
-  bool flag1 ;
-  bool flag2 ;
-
+    
   int P;
-  
+  int DeltaT=0;
+  int T1;
+  int T2;
+
+  int flag;
 
 };
 
